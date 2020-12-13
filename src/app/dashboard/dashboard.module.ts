f5ba8ef4b708e13d6ce4758f6e606a1365615a1f
Shobhit Srivastava <shosrien@in.ibm.com>
2020-12-13 13:10:38 +0530

    skeleton

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DashboardRoutingModule
+  ],
+  declarations: [DashboardComponent, HomeComponent]
+})
+export class DashboardModule { }
