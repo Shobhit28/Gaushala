c5252523f949908c69955648ab1de4c45bac4e4a
Shikhar Kulshrestha <shikhar.kulshrestha1105@gmail.com>
2021-01-03 18:18:50 +0530

    Not-Working-G-Signin

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -9,6 +9,14 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  onSignIn(googleUser) {
+  let profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
   ngOnInit() {
   }
 
